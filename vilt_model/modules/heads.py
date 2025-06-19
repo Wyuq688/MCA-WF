ef75ac1b12b0c67ae92bc18fa0406b97deff1b28
Wyuq688 <74965688+Wyuq688@users.noreply.github.com>
2025-06-19 18:51:42 +0800

    Add files via upload

---
+++ vilt_model/modules/heads.py
@@ -0,0 +1,69 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import ipdb
+
+from transformers.models.bert.modeling_bert import BertPredictionHeadTransform
+
+
+class Pooler(nn.Module):#提取cls token（B，C）得到分类特征
+    def __init__(self, hidden_size):
+        super().__init__()
+        self.dense = nn.Linear(hidden_size, hidden_size)
+        self.activation = nn.Tanh()
+
+    def forward(self, hidden_states):
+        ipdb.set_trace()
+        pooled_output = hidden_states[:, 0]
+        pooled_output = self.dense(pooled_output)
+        pooled_output = self.activation(pooled_output)
+        return pooled_output
+class Mean(nn.Module):#将feature map平均池化得到分类特征
+    def __init__(self, hidden_size):
+        super().__init__()
+        self.dense = nn.Linear(hidden_size, hidden_size)
+        self.activation = nn.Tanh()
+
+    def forward(self, hidden_states):
+        ipdb.set_trace()
+        output = hidden_states.mean(dim=1)
+        output = self.dense(output)
+        output = self.activation(pooled_output)
+        return output
+
+
+class ITMHead(nn.Module):
+    def __init__(self, hidden_size):
+        super().__init__()
+        self.fc = nn.Linear(hidden_size, 2)
+
+    def forward(self, x):
+        x = self.fc(x)
+        return x
+
+
+class MLMHead(nn.Module):
+    def __init__(self, config, weight=None):
+        super().__init__()
+        self.transform = BertPredictionHeadTransform(config)
+        self.decoder = nn.Linear(config.hidden_size, config.vocab_size, bias=False)
+        self.bias = nn.Parameter(torch.zeros(config.vocab_size))
+        if weight is not None:
+            self.decoder.weight = weight
+
+    def forward(self, x):
+        x = self.transform(x)
+        x = self.decoder(x) + self.bias
+        return x
+
+
+class MPPHead(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.transform = BertPredictionHeadTransform(config)
+        self.decoder = nn.Linear(config.hidden_size, 256 * 3)
+
+    def forward(self, x):
+        x = self.transform(x)
+        x = self.decoder(x)
+        return x
